57c6e69bd06d621461a8af99574b069259de3c4f
김경민 <fktkzmfk707@gmail.com>
2025-01-10 08:58:22 +0900

    최신

--- workspace_html/gonbu/js/naver.js
+++ workspace_html/gonbu/js/naver.js
@@ -1,10 +1,25 @@
-// document.addEventListener("load", function() {
+window.addEventListener("load",init)
+function init(){
+
+
+
     
+    document.querySelector("#text_write_button").addEventListener('click',function(){
+        let text_write1 = document.querySelector('#maincontent_text_write1').value
+        let text_write2 = document.querySelector('#maincontent_text_write2').value
+        console.log("text_write1 :",text_write1);
+        console.log("text_write2 :",text_write2);
+
+        let text_paste_copy 
 
 
 
 
 
+        
+        let text_paste = document.querySelector("#maincontent_text_paste")   
+    })
+    
+}
 
 
-// }
