cea3d2413497a7480ecac37f5fc46cdab3f66a99
김경민 <fktkzmfk707@gmail.com>
2025-04-08 15:43:15 +0900

    c#1차로

---
+++ workspace_cs/ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Microsoft.VisualBasic;
+
+namespace kr.or.human
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World");
+            
+        }
+    }
+}
